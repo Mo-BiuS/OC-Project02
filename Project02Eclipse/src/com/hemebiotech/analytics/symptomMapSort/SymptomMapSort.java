4f49d57807078da16de6f5b3da5683588a0dfd49
Mo-BiuS <lodland95@gmail.com>
2021-07-15 12:36:42 +0200

    MapSort

--- Project02Eclipse/src/com/hemebiotech/analytics/symptomMapSort/SymptomMapSort.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/symptomMapSort/SymptomMapSort.java
@@ -1,6 +1,8 @@
 package com.hemebiotech.analytics.symptomMapSort;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SymptomMapSort implements ISymptomMapSort {
 	
@@ -25,8 +27,28 @@ public class SymptomMapSort implements ISymptomMapSort {
 
 	@Override
 	public Map<String, Integer> getSortedMap() {
-		// TODO Auto-generated method stub
-		return map;
+		if(sortType != null) {
+			switch (sortType) {
+			case KeyAscending:
+				return sortKeyAscending();
+			case ValueAscending:
+				return sortValueAscending();
+			default:
+				return null;
+			}
+		}else return null;
 	}
 
+	private Map<String, Integer> sortKeyAscending() {
+		return map.entrySet().stream()
+			   .sorted(Map.Entry.comparingByKey()) 			
+			   .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+			   (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+	}
+	private Map<String, Integer> sortValueAscending() {
+		return map.entrySet().stream()
+				  .sorted(Map.Entry.comparingByValue()) 			
+				  .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+				  (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+	}
 }
